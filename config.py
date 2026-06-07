a6ce961efcd27c135be57ec6ea776efaf011989c
zdowdy <zdowdy@mail.valenciacollege.edu>
2026-06-07 18:51:16 -0400

    Created data for brand_focus DB

--- config.py
+++ config.py
@@ -17,6 +17,39 @@ REQUEST_HEADERS = {                                                     #very im
         "Chrome/124.0.0.0 Safari/537.36"
     )
 }
+BRANDS = [
+    {'brand_name': 'Corsair',   'market_focus':'both'},
+    {'brand_name': 'G.SKILL',   'market_focus':'consumer'},
+    {'brand_name': 'Kingston Technology Corp.',   'market_focus':'both'},
+    {'brand_name': 'Crucial',   'market_focus':'enterprise'},
+    {'brand_name': 'Black Diamond Memory',   'market_focus':'both'},
+    {'brand_name': 'Team Group',   'market_focus':'both'},
+    {'brand_name': 'A-Tech',   'market_focus':'both'},
+    {'brand_name': 'SAMSUNG',   'market_focus':'enterprise'},
+    {'brand_name': 'NEMIX RAM',   'market_focus':'both'},
+    {'brand_name': 'V-color',   'market_focus':'both'},
+    {'brand_name': 'Patriot Memory',   'market_focus':'both'},
+    {'brand_name': 'Timetec',   'market_focus':'consumer'},
+    {'brand_name': 'SK hynix',   'market_focus':'enterprise'},
+    {'brand_name': 'RIMLANCE',   'market_focus':'enterprise'},
+    {'brand_name': 'OWC',   'market_focus':'both'},
+    {'brand_name': 'Silicon Power',   'market_focus':'both'},
+    {'brand_name': 'DELL',   'market_focus':'both'},
+    {'brand_name': 'XPG',   'market_focus':'both'},
+    {'brand_name': 'PNY Technologies, INC.',   'market_focus':'both'},
+    {'brand_name': 'Micron Technologies, INC.',   'market_focus':'enterprise'},
+    {'brand_name': 'KLEVV',   'market_focus':'consumer'},
+    {'brand_name': 'KINGBANK',   'market_focus':'consumer'},
+    {'brand_name': 'DATO',   'market_focus':'consumer'},
+    {'brand_name': 'ADATA',   'market_focus':'both'},
+    {'brand_name': 'HP',   'market_focus':'both'},
+    {'brand_name': 'Gigastone Corporation',   'market_focus':'consumer'},
+    {'brand_name': 'Brute Networks',   'market_focus':'enterprise'},
+    {'brand_name': 'Micron',   'market_focus':'enterprise'},
+    {'brand_name': 'KingSpec',   'market_focus':'consumer'},
+    {'brand_name': 'KINGSMAN GAMING',   'market_focus':'consumer'},
+    {'brand_name': 'Ballistix',   'market_focus':'consumer'}
+]
 
 #Scheduler
 SCRAPE_INTERVAL_HOURS = 1                                             #How often the scraper gets ran
